e4a1f2a5842de11e973db4af354bdf6f747b3314
Gennadiy Koganovich <genakoganovich@gmail.com>
2020-10-16 17:27:41 +0300

    state 4/7 done

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,24 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
-print('Type "en" if you want to translate from French into English, '
-      'or "fr" if you want to translate from English into French:')
-language = 'french'
-# language = input()
+languages = [None, 'Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew',
+             'Japanese', 'Dutch', 'Polish', 'Portuguese', 'Romanian', 'Russian', 'Turkish']
+print("Hello, you're welcome to the translator. Translator supports:")
+for i in range(1, len(languages)):
+    print('{}. {}'.format(i, languages[i]))
+
+print('Type the number of your language:')
+from_number = int(input())
+# from_number = 3
+print('Type the number of language you want to translate to:')
+to_number = int(input())
+# to_number = 5
 print('Type the word you want to translate:')
-word = 'hello'
-# word = input()
-print('You chose "{}" as the language to translate "{}" to.'.format(language, word))
-url = 'https://context.reverso.net/translation/english-{}/{}'.format(language, word)
+# word = 'hello'
+word = input()
+url = 'https://context.reverso.net/translation/{}-{}/{}'.\
+    format(languages[from_number].lower(), languages[to_number].lower(), word)
 headers = {'user-agent': 'my-app/0.0.1'}
 response = requests.get(url, headers=headers)
-print(response.status_code, 'OK')
-
 soup = BeautifulSoup(response.content, 'html.parser')
 found_words = [item.text.strip() for item in soup.find_all('a', {'class': 'translation'})][1:6]
-print('\nContext examples:\n')
-print('French Translations:')
+print('\n{} Translations:'.format(languages[to_number]))
 print(*found_words, sep='\n')
-print('\nFrench Examples:')
+print('\n{} Examples:'.format(languages[to_number]))
 found_examples = soup.find_all('div', {'class': 'example'})
 examples = list()
 for item in found_examples:

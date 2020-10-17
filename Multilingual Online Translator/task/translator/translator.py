d16704fb02fae36c0db7bd6b4976c93cb034a02d
Gennadiy Koganovich <genakoganovich@gmail.com>
2020-10-17 14:45:33 +0300

    state 5/7 done

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 languages = [None, 'Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew',
@@ -28,22 +29,35 @@ def print_results(found_words, examples, to_num):
         print()
 
 
+def print_translation(from_number, to_number, word):
+    found_words, examples = translate(from_number, to_number, word)
+    print_results(found_words, examples, to_number)
+    original_stdout = sys.stdout
+    with open(word + '.txt', 'a') as f_out:
+        sys.stdout = f_out
+        print_results(found_words, examples, to_number)
+        sys.stdout = original_stdout
+
+
 def run():
     print("Hello, you're welcome to the translator. Translator supports:")
     for i in range(1, len(languages)):
         print('{}. {}'.format(i, languages[i]))
 
     print('Type the number of your language:')
-    # from_number = int(input())
-    from_number = 3
+    from_number = int(input())
+    # from_number = 3
     print('Type the number of language you want to translate to:')
-    # to_number = int(input())
-    to_number = 5
+    to_number = int(input())
+    # to_number = 0
     print('Type the word you want to translate:')
-    # word = input()
-    word = 'hello'
-    found_words, examples = translate(from_number, to_number, word)
-    print_results(found_words, examples, to_number)
+    word = input()
+    # word = 'hello'
+    if to_number:
+        print_translation(from_number, to_number, word)
+    else:
+        for i in range(1, len(languages)):
+            print_translation(from_number, i, word)
 
 
 run()
